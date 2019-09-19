bdac114e595d9e3ef0d8a85cf78b354c4d239873
Tony Yang <tonyyang1029@gmail.com>
2019-09-19 18:41:09 +0800

    Initial commit

---
+++ app/src/main/java/com/ty/android/ndkdemo2/MainActivity.java
@@ -0,0 +1,23 @@
+package com.ty.android.ndkdemo2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.ty.android.ndkdemo.NdkLib;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextView = null;
+    private NdkLib   mNdkLib   = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNdkLib = new NdkLib();
+        mTextView = findViewById(R.id.ID_MAIN_TEXTVIEW);
+        mTextView.setText(mNdkLib.getStringFromNdk());
+    }
+}
